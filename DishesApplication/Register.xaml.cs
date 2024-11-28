18645a2fd74c5d7b730f287721147ba7b6a69abf
silly-person-on-the-cheese-planet <nbnbnb-1212ffq@yandex.ru>
2024-11-28 23:37:37 +0300

    v2.2

--- DishesApplication/Register.xaml.cs
+++ DishesApplication/Register.xaml.cs
@@ -24,7 +24,7 @@ namespace DishesApplication
             InitializeComponent();
         }
 
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
